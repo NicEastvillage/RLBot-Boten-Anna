0f9f939b19857e6e299639c8493a5f294aabc121
Mikkel Kuntz <mkuntz17@student.aau.dk>
2018-02-20 11:43:14 +0100

    Added car rotation to statusWindow and tries to take player status (max 2 cars)

--- java/src/botenanna/overlayWindow/Window.java
+++ java/src/botenanna/overlayWindow/Window.java
@@ -3,6 +3,7 @@ package botenanna.overlayWindow;
 //https://docs.oracle.com/javase/tutorial/uiswing/examples/layout/FlowLayoutDemoProject/src/layout/FlowLayoutDemo.java
 
 import botenanna.math.Vector3;
+import javafx.scene.text.FontWeight;
 import rlbot.api.GameData;
 
 import javax.swing.*;
@@ -12,23 +13,26 @@ public class Window extends JFrame{
 
     private JLabel carLocation;
     private JLabel carVelocity;
+    private JLabel carRotation;
     private JLabel ballLocation;
     private JLabel ballVelocity;
-
-    //FlowLayout windowLayout = new FlowLayout();
-    final String LtoR = "Left to right";
+    private JLabel angleToBall;
 
     public Window(){
         //Creating window frame
         JFrame frame = new JFrame("Status Window"); //Creating the frame
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setAlwaysOnTop(true);
+
         frame.setResizable(false);
 
         //Setting up content pane (adding stuff)
         addComponentsToPane(frame.getContentPane());
 
         //Display window
+
         frame.pack();
+        frame.setSize(400,150);
         frame.setVisible(true);
 
     }
@@ -39,21 +43,29 @@ public class Window extends JFrame{
         JPanel line2 = new JPanel();
         JPanel line3 = new JPanel();
         JPanel line4 = new JPanel();
+        JPanel line5 = new JPanel();
+        JPanel line6 = new JPanel();
 
         line1.setLayout(new FlowLayout(FlowLayout.LEFT));
         line2.setLayout(new FlowLayout(FlowLayout.LEFT));
         line3.setLayout(new FlowLayout(FlowLayout.LEFT));
         line4.setLayout(new FlowLayout(FlowLayout.LEFT));
+        line5.setLayout(new FlowLayout(FlowLayout.LEFT));
+        line6.setLayout(new FlowLayout(FlowLayout.LEFT));
 
         carLocation = new JLabel("Car Location(x, y, z)");
         carVelocity = new JLabel("Car Velocity(x, y, z)");
+        carRotation = new JLabel("Car Rotation(Pitch, Yaw, Roll)");
         ballLocation = new JLabel("Ball Location(x, y, z)");
         ballVelocity = new JLabel("Ball Velocity(x, y, z)");
+        angleToBall = new JLabel("Angle to ball()");
 
         line1.add(carLocation);
         line2.add(carVelocity);
-        line3.add(ballLocation);
-        line4.add(ballVelocity);
+        line3.add(carRotation);
+        line4.add(ballLocation);
+        line5.add(ballVelocity);
+        line6.add(angleToBall);
 
         JPanel mainPanel = new JPanel();
         mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS)); //Stack on Y_axis, aka stack
@@ -62,6 +74,8 @@ public class Window extends JFrame{
         mainPanel.add(line2);
         mainPanel.add(line3);
         mainPanel.add(line4);
+        mainPanel.add(line5);
+        mainPanel.add(line6);
 
         pane.add(mainPanel);
     }
@@ -69,11 +83,20 @@ public class Window extends JFrame{
     //Updates the labels with data from gamePacket
     public void updateData(GameData.GameTickPacket gameDataPacket){
         //Player info:
+
         GameData.PlayerInfo player = gameDataPacket.getPlayers(0); //Gets the player info
+
+        //Find player: //TODO: not a good solution
+        if(player.getIsBot())
+            player = gameDataPacket.getPlayers(1);
+
+
         Vector3 playerLocationVector = Vector3.convert(player.getLocation()); //Gets player location vector
         updateLabelCarLocation(playerLocationVector);//Updating car location label
         Vector3 playerVelocityVector = Vector3.convert(player.getVelocity()); //Gets player velocity vector
         updateLabelCarVelocity(playerVelocityVector); //Update car velocity label
+        Vector3 playerRotationVector = Vector3.convert(player.getVelocity());
+        updateLabelCarRotation(playerRotationVector); //Update car rotation label
 
         //Ball info:
         GameData.BallInfo ball = gameDataPacket.getBall(); //Gets ball info
@@ -81,6 +104,8 @@ public class Window extends JFrame{
         updateLabelBallLocation(ballLocationVector); //Updating ball location label
         Vector3 ballVelocityVector = Vector3.convert(ball.getVelocity()); //Gets ball velocity vector
         updateLabelBallVelocity(ballVelocityVector); //Update ball velocity label
+
+
 
     }
 
@@ -102,6 +127,15 @@ public class Window extends JFrame{
         carVelocity.setText("Car Velocity (x: " + playVelX + ", y: " + playVelY + ", z: " + playVelZ + ")");
     }
 
+    private void updateLabelCarRotation(Vector3 playerRotationVector) {
+        double playRotX, playRotY, playRotZ;
+        playRotX = playerRotationVector.x;
+        playRotY = playerRotationVector.y;
+        playRotZ = playerRotationVector.z;
+
+        carRotation.setText("Car Rotation (Pitch: " + playRotX + ", Yaw: " + playRotY + ", Roll: " + playRotZ + ")");
+    }
+
     private void updateLabelBallLocation(Vector3 ballVecLocation){
         double ballX, ballY, ballZ;
         ballX = ballVecLocation.x;
@@ -118,5 +152,9 @@ public class Window extends JFrame{
         ballVelZ = ballVecVelocity.z;
 
         ballVelocity.setText("Ball Velocity (x: " + ballVelX + ", y: " + ballVelY + ", z: " + ballVelZ + ")");
+    }
+
+    private void updateLabelAngleToBall(){
+
     }
 }
