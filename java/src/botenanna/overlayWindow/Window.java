2d526d6896c26cd5d21e9b05c68bf8da1b025aab
Mikkel Kuntz <mkuntz17@student.aau.dk>
2018-02-21 12:23:16 +0100

    Added the final touch to the window for now. Including cleaing the code and adding comments all over. Removed irrelevant window tests. Deleted main used in debugging.

--- java/src/botenanna/overlayWindow/Window.java
+++ java/src/botenanna/overlayWindow/Window.java
@@ -1,17 +1,14 @@
 package botenanna.overlayWindow;
 
-//https://docs.oracle.com/javase/tutorial/uiswing/examples/layout/FlowLayoutDemoProject/src/layout/FlowLayoutDemo.java
-
-import botenanna.math.Vector3;
+import botenanna.math.*;
 import rlbot.api.GameData;
 
 import javax.swing.*;
 import java.awt.*;
-import java.text.DecimalFormat;
 
+/** Debug/Status window. Displays a window that shows the stats of a car and the ball. */
 public class Window extends JFrame{
 
-    private static DecimalFormat df2 = new DecimalFormat(".##");
     private static Font font = new Font("Courier New", Font.PLAIN , 12);
 
     private JLabel carLocation;
@@ -21,27 +18,26 @@ public class Window extends JFrame{
     private JLabel ballVelocity;
     private JLabel angleToBall;
 
+    /** Creating the window adding content/layout */
     public Window(){
         //Creating window frame
         JFrame frame = new JFrame("Status Window"); //Creating the frame
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setAlwaysOnTop(true);
-
         frame.setResizable(false);
 
         //Setting up content pane (adding stuff)
         addComponentsToPane(frame.getContentPane());
 
-        //Display window
-
+        //Display the window
         frame.pack();
         frame.setSize(440,190);
         frame.setVisible(true);
-
     }
 
-    //Adds components to the pane
+    /** Adds components/content to to the frame */
     private void addComponentsToPane(final Container pane){
+        //Creating panels, one for each label (needed for pretty layout)
         JPanel line1 = new JPanel();
         JPanel line2 = new JPanel();
         JPanel line3 = new JPanel();
@@ -49,6 +45,7 @@ public class Window extends JFrame{
         JPanel line5 = new JPanel();
         JPanel line6 = new JPanel();
 
+        //Setting layout and alignment to left
         line1.setLayout(new FlowLayout(FlowLayout.LEFT));
         line2.setLayout(new FlowLayout(FlowLayout.LEFT));
         line3.setLayout(new FlowLayout(FlowLayout.LEFT));
@@ -56,6 +53,7 @@ public class Window extends JFrame{
         line5.setLayout(new FlowLayout(FlowLayout.LEFT));
         line6.setLayout(new FlowLayout(FlowLayout.LEFT));
 
+        //Adding default content to labels
         carLocation = new JLabel("Car Location(x, y, z)");
         carVelocity = new JLabel("Car Velocity(x, y, z)");
         carRotation = new JLabel("Car Rotation(Pitch, Yaw, Roll)");
@@ -63,6 +61,7 @@ public class Window extends JFrame{
         ballVelocity = new JLabel("Ball Velocity(x, y, z)");
         angleToBall = new JLabel("Angle to ball()");
 
+        //Setting the font (monospaced)
         carLocation.setFont(font);
         carVelocity.setFont(font);
         carRotation.setFont(font);
@@ -70,6 +69,7 @@ public class Window extends JFrame{
         ballVelocity.setFont(font);
         angleToBall.setFont(font);
 
+        //Adding the labels to the panels
         line1.add(carLocation);
         line2.add(carVelocity);
         line3.add(carRotation);
@@ -77,6 +77,7 @@ public class Window extends JFrame{
         line5.add(ballVelocity);
         line6.add(angleToBall);
 
+        //Creating main panel and adding label panels (stack on y-axis)
         JPanel mainPanel = new JPanel();
         mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS)); //Stack on Y_axis, aka stack
         mainPanel.setAlignmentX(Component.LEFT_ALIGNMENT);
@@ -87,26 +88,21 @@ public class Window extends JFrame{
         mainPanel.add(line5);
         mainPanel.add(line6);
 
+        //Adding main panel to frame
         pane.add(mainPanel);
     }
 
-    //Updates the labels with data from gamePacket
+    /** Updates the labels with the given game data.
+     * @param gameDataPacket a packet with information about the current state of the game. */
     public void updateData(GameData.GameTickPacket gameDataPacket){
-        //Player info:
-
-        GameData.PlayerInfo player = gameDataPacket.getPlayers(0); //Gets the player info
-
-        //Find player: //TODO: not a good solution
-        if(player.getIsBot())
-            player = gameDataPacket.getPlayers(1);
-
-
-        Vector3 playerLocationVector = Vector3.convert(player.getLocation()); //Gets player location vector
-        updateLabelCarLocation(playerLocationVector);//Updating car location label
-        Vector3 playerVelocityVector = Vector3.convert(player.getVelocity()); //Gets player velocity vector
-        updateLabelCarVelocity(playerVelocityVector); //Update car velocity label
-        Vector3 playerRotationVector = Vector3.convert(player.getVelocity());
-        updateLabelCarRotation(playerRotationVector); //Update car rotation label
+        //Car info:
+        GameData.PlayerInfo car = gameDataPacket.getPlayers(0); //Gets the car info
+        Vector3 carLocationVector = Vector3.convert(car.getLocation()); //Gets car location vector
+        updateLabelCarLocation(carLocationVector);//Updating car location label
+        Vector3 carVelocityVector = Vector3.convert(car.getVelocity()); //Gets car velocity vector
+        updateLabelCarVelocity(carVelocityVector); //Update car velocity label
+        Vector3 carRotationVector = Vector3.convert(car.getVelocity()); //Gets car rotation vector
+        updateLabelCarRotation(carRotationVector); //Update car rotation label
 
         //Ball info:
         GameData.BallInfo ball = gameDataPacket.getBall(); //Gets ball info
@@ -115,39 +111,67 @@ public class Window extends JFrame{
         Vector3 ballVelocityVector = Vector3.convert(ball.getVelocity()); //Gets ball velocity vector
         updateLabelBallVelocity(ballVelocityVector); //Update ball velocity label
 
-
-
+        //Angle between car and ball:
+        updateLabelAngleToBall(RLMath.carsAngleToPoint(new Vector2(carLocationVector), carRotationVector.y, new Vector2(ballLocationVector)));
     }
 
+    /** Updates the car location label with a formatted string.
+     * @param playerVecLocation a 3D-vector containing coordinates for the players location. */
     public void updateLabelCarLocation(Vector3 playerVecLocation){
         carLocation.setText(formatLabelVectorXYZ("Car Location", playerVecLocation));
     }
 
+    /** Updates the car velocity label with formatted string.
+     * @param playerVecVelocity a 3D-vector containing the velocity of the player. */
     public void updateLabelCarVelocity(Vector3 playerVecVelocity){
         carVelocity.setText(formatLabelVectorXYZ("Car Velocity", playerVecVelocity));
 
     }
 
+    /** Updates the car rotation label with formatted string.
+     * @param playerRotationVector a 3D-vector containing the rotation of the player. */
     public void updateLabelCarRotation(Vector3 playerRotationVector) {
         carRotation.setText(formatLabelVectorRotation("Car Rotation", playerRotationVector));
     }
 
+    /** Updates the ball location label with a formatted string.
+     * @param ballVecLocation a 3D-vector containing the coordinates of the ball. */
     public void updateLabelBallLocation(Vector3 ballVecLocation){
         ballLocation.setText(formatLabelVectorXYZ("Ball Location", ballVecLocation));
     }
 
+    /** Updates the ball velocity label with a formatted string.
+     * @param ballVecVelocity a 3D-vector containing the velocity of the ball. */
     public void updateLabelBallVelocity(Vector3 ballVecVelocity){
         ballVelocity.setText(formatLabelVectorXYZ("Ball Velocity", ballVecVelocity));
     }
 
-    public void updateLabelAngleToBall(){
-
+    /** Updates the angle to ball label with a formatted string.
+     * @param angle the angle between the ball and car. */
+    public void updateLabelAngleToBall(double angle){
+        angleToBall.setText(formatLabelSingleValue("Angle", angle));
     }
 
+    /** Formats a string to be used in a label.
+     * @param labelName a string used to describe the value.
+     * @param value a value to be used in the string.
+     * @return a formatted string used in a label. */
+    private String formatLabelSingleValue(String labelName, double value){
+        return labelName + String.format(" (% 4.2f)", value);
+    }
+
+    /** Formats a string to be used in a label.
+     * @param labelName a string used to describe the vector.
+     * @param vector a 3D-vector.
+     * @return a formatted string used in a label. */
     private String formatLabelVectorXYZ(String labelName, Vector3 vector){
         return labelName + String.format(" (x:% 8.2f, y:% 8.2f, z:% 8.2f)", vector.x, vector.y, vector.z);
     }
 
+    /** Formats a string used in a label to describe rotation (pitch, yaw and roll).
+     * @param labelName a string used to describe the vector.
+     * @param vector a 3D-vector vector describing pitch, yaw and roll.
+     * @return a formatted string used in labels describing rotation. */
     private String formatLabelVectorRotation(String labelName, Vector3 vector){
         return labelName + String.format(" (Pitch:% 8.2f, Yaw:% 8.2f, Roll:% 8.2f)", vector.x, vector.y, vector.z);
     }
