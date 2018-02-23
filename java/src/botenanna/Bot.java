841f50912c47bed51312553b0c0410ae762c625b
Christopher Buchardt <cbucha17@student.aau.dk>
2018-02-23 14:04:24 +0100

    Fixed some math

--- java/src/botenanna/Bot.java
+++ java/src/botenanna/Bot.java
@@ -20,16 +20,19 @@ public class Bot {
         team = (teamIndex == 0 ? Team.BLUE : Team.ORANGE);
     }
 
-    /** Let the bot process the information from the data packet
+    /**
+     * Let the bot process the information from the data packet
+     *
      * @param packet the game tick packet from the game
-     * @return an AgentOutput of what the agent want to do */
+     * @return an AgentOutput of what the agent want to do
+     */
     public AgentOutput process(GameData.GameTickPacket packet) {
 
         // TODO Go towards where the ball will land!
         GameData.BallInfo ball = packet.getBall();
         Vector3 ballPos = Vector3.convert(ball.getLocation());
         Vector3 ballVel = Vector3.convert(ball.getVelocity());
-        Vector2 ballBuff = new Vector2(0,400);
+        Vector2 ballBuff = new Vector2(0, 600);
 
         // Where will ball the land?
         Vector2 ballLandingPos = ballPos.asVector2(); // this is default, if ball is not "landing" anywhere
@@ -40,11 +43,11 @@ public class Bot {
         double landingTime = ballBody.predictArrivalAtHeight(92); // TODO: BALL RADIUS = 92 uu
         boolean home;
 
-        if (team == Team.BLUE) { //If the player is blue then they will  return "home" if they are on the wrong side of it
-            home = goingHome(ballLandingPos,ballBuff, Vector3.convert(packet.getPlayers(playerIndex).getLocation()).asVector2(),true);
-            if (home) {
-                return goTowardsPoint(packet, new Vector2(0, -5120));
+         //If the player is blue then they will  return "home" if they are on the wrong side of it
+            home = goingHome(ballLandingPos, ballBuff, Vector3.convert(packet.getPlayers(playerIndex).getLocation()).asVector2(), teamsDirectionToGoal(team));
 
+            if (home) {
+                return goTowardsPoint(packet, new Vector2(0, 5120*teamsDirectionToGoal(team)));
             }
             if (!Double.isNaN(landingTime)) {
                 ballBody.step(landingTime);
@@ -52,25 +55,14 @@ public class Bot {
             }
             return goTowardsPoint(packet, ballLandingPos);
 
-        }  else {
-            home = goingHome(ballLandingPos,ballBuff,Vector3.convert(packet.getPlayers(playerIndex).getLocation()).asVector2(),false);
-            if (home) {
-                return goTowardsPoint(packet, new Vector2(0, 5120));
 
-            }
-            if (!Double.isNaN(landingTime)) {
-                    ballBody.step(landingTime);
-                    ballLandingPos = ballBody.getPosition().asVector2();
-            }
-            return goTowardsPoint(packet, ballLandingPos);
+    }
 
-        }
-
-}
     /**
      * @param packet the game tick packet from the game
-     * @param point the location the agent should go towards
-     * @return an AgentOutput of what the agent needs to do to get to the point */
+     * @param point  the location the agent should go towards
+     * @return an AgentOutput of what the agent needs to do to get to the point
+     */
     private AgentOutput goTowardsPoint(GameData.GameTickPacket packet, Vector2 point) {
 
         // TODO For now we always to full throttle forwards, though that not be the shortest route. Maybe we should slide in some cases?
@@ -89,23 +81,19 @@ public class Bot {
         return new AgentOutput().withAcceleration(1).withSteer(steering);
 
     }
-    private boolean goingHome (Vector2 ball, Vector2 player, Vector2 ballBuff,  boolean color ){
-        if(color){
-        if (ball.y-ballBuff.y < player.y){
+
+    private boolean goingHome(Vector2 ball, Vector2 player, Vector2 ballBuff, int color) {
+        //Need to fix the math to not be double.
+        if (ball.y + (ballBuff.y*color) < -player.y*color) {
             return true;
-        }else if (ball.y-ballBuff.y > player.y && (ball.y-ballBuff.y) > ballBuff.y){
+        } else if (ball.y + ballBuff.y*color > -player.y*color && (ball.y + (ballBuff.y*color)) > ballBuff.y) {
             return false;
-        }
-        else return false;
+        } else return false;
+    }
 
-    }   else
-            if (ball.y+ballBuff.y > player.y){
-                return true;
-            }else if (ball.y+ballBuff.y < player.y && (ball.y+ballBuff.y) < ballBuff.y){
-                return false;
-            }
-            else return false;
 
+    public static int teamsDirectionToGoal(Team team) {
+        return team == Team.BLUE ? -1 : 1;
     }
 }
 
