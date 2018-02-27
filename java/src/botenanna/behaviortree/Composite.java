4c961f76af52967c5af05181d305ad8d4d65f21f
NicEastvillage <nicoesterby@gmail.com>
2018-02-27 17:10:33 +0100

    Added the Composite node type

---
+++ java/src/botenanna/behaviortree/Composite.java
@@ -0,0 +1,16 @@
+package botenanna.behaviortree;
+
+import java.util.ArrayList;
+
+/** Composites are nodes that have one or more child nodes.*/
+public abstract class Composite implements Node {
+
+    /** A list of all children this composite node has. */
+    protected ArrayList<Node> children = new ArrayList<>();
+
+    /** Add a child to this Composite node */
+    @Override
+    public final void addChild(Node child) throws BehaviourTreeBuildingException {
+
+    }
+}
