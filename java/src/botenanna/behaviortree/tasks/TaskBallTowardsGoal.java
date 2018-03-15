a491f897bd11938ee131224ad8589946ad84f791
Hindsgaul <mhinds17@student.aau.dk>
2018-03-15 14:18:44 +0100

    adjusted variables

--- java/src/botenanna/behaviortree/tasks/TaskBallTowardsGoal.java
+++ java/src/botenanna/behaviortree/tasks/TaskBallTowardsGoal.java
@@ -38,18 +38,18 @@ public class TaskBallTowardsGoal extends Leaf {
         // TODO For now we always to full throttle forwards, though that not be the shortest route. Maybe we should slide in some cases?
         // TODO Also, the bot will overshoot. In some cases we want the bot to stop, or get to pointFunc at a specific time (e.g. when ball lands)
 
-        Vector3 expectedBallLocation = input.ballLocation.plus(input.ballVelocity.scale(0.5));
+        Vector3 expectedBallLocation = input.ballLocation.plus(input.ballVelocity.scale(0.4));
 
         // Creates Vector needed to adjust shooting depended on left and right goal post
         Vector2 ballToRightGoalPostVector = AgentInput.BLUE_GOALPOST_RIGHT.minus(expectedBallLocation.asVector2());
         ballToRightGoalPostVector = ballToRightGoalPostVector.getNormalized();
-        ballToRightGoalPostVector = ballToRightGoalPostVector.scale(-80);
+        ballToRightGoalPostVector = ballToRightGoalPostVector.scale(-85);
         ballToRightGoalPostVector = ballToRightGoalPostVector.plus(expectedBallLocation.asVector2());
 
         // Creates Vector needed to adjust shooting depended on left and right goal post
         Vector2 ballToLeftGoalPostVector = AgentInput.BLUE_GOALPOST_LEFT.minus(expectedBallLocation.asVector2());
         ballToLeftGoalPostVector = ballToLeftGoalPostVector.getNormalized();
-        ballToLeftGoalPostVector = ballToLeftGoalPostVector.scale(-80);
+        ballToLeftGoalPostVector = ballToLeftGoalPostVector.scale(-85);
         ballToLeftGoalPostVector = ballToLeftGoalPostVector.plus(expectedBallLocation.asVector2());
 
         // Get the needed positions and rotations
