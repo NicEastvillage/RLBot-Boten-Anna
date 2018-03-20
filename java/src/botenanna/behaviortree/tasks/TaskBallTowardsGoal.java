148edd5a89864558decddc5657f64e58774e99e7
Hindsgaul <mhinds17@student.aau.dk>
2018-03-20 09:30:50 +0100

    Adjusted variables

--- java/src/botenanna/behaviortree/tasks/TaskBallTowardsGoal.java
+++ java/src/botenanna/behaviortree/tasks/TaskBallTowardsGoal.java
@@ -33,7 +33,7 @@ public class TaskBallTowardsGoal extends Leaf {
         // TODO Else try the difference of acceleration on car and ball vector with directions(and distance), and multiply/divide with seconds the predict
         // TODO If balls vector towards goal is bad adjust car before shooting.
 
-        double predictSeconds = (input.ballVelocity.getMagnitude()/input.myVelocity.getMagnitude())*(input.myDistanceToBall/2200);
+        double predictSeconds = (input.ballVelocity.getMagnitude()/input.myVelocity.getMagnitude())*(input.myDistanceToBall/1800);
 
         //double predictSeconds = (input.myDistanceToBall/2200);
 
@@ -46,10 +46,10 @@ public class TaskBallTowardsGoal extends Leaf {
        }
 
         if (1 < input.angleToBall || input.angleToBall < -1) {
-            predictSeconds = 1;
+            predictSeconds = 0.1;
         }
 
-        if (input.myDistanceToBall < 250) {
+        if (input.myDistanceToBall < 300) {
             predictSeconds = 0;
         }
 
