0345a35051678e52ac4c6642d185cab2a6fb286a
Hindsgaul <mhinds17@student.aau.dk>
2018-03-20 09:19:32 +0100

    No prediction under 300 distance to ball

--- java/src/botenanna/behaviortree/tasks/TaskBallTowardsGoal.java
+++ java/src/botenanna/behaviortree/tasks/TaskBallTowardsGoal.java
@@ -33,9 +33,9 @@ public class TaskBallTowardsGoal extends Leaf {
         // TODO Else try the difference of acceleration on car and ball vector with directions(and distance), and multiply/divide with seconds the predict
         // TODO If balls vector towards goal is bad adjust car before shooting.
 
-        //double predictSeconds = (input.ballVelocity.getMagnitude()/input.myVelocity.getMagnitude())*(input.myDistanceToBall/2200);
+        double predictSeconds = (input.ballVelocity.getMagnitude()/input.myVelocity.getMagnitude())*(input.myDistanceToBall/2200);
 
-        double predictSeconds = (input.myDistanceToBall/2200);
+        //double predictSeconds = (input.myDistanceToBall/2200);
 
        // if(input.myDistanceToBall < 250) {
        //     predictSeconds = 0.1;
@@ -47,6 +47,10 @@ public class TaskBallTowardsGoal extends Leaf {
 
         if (1 < input.angleToBall || input.angleToBall < -1) {
             predictSeconds = 1;
+        }
+
+        if (input.myDistanceToBall < 250) {
+            predictSeconds = 0;
         }
 
         Vector3 expectedBallLocation = input.ballLocation.plus(input.ballVelocity.scale(predictSeconds));
