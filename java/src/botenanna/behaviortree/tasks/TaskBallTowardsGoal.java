1b7fb1e15663b2fe74deecca2652f21d2deb92c9
Hindsgaul <mhinds17@student.aau.dk>
2018-03-20 08:44:05 +0100

    Made a print to check ball velocity for testing

--- java/src/botenanna/behaviortree/tasks/TaskBallTowardsGoal.java
+++ java/src/botenanna/behaviortree/tasks/TaskBallTowardsGoal.java
@@ -49,6 +49,8 @@ public class TaskBallTowardsGoal extends Leaf {
             predictSeconds = 1;
         }
 
+        System.out.println(input.ballVelocity.getMagnitude());
+
         Vector3 expectedBallLocation = input.ballLocation.plus(input.ballVelocity.scale(predictSeconds));
 
         Vector2 ballToRightGoalPostVector = new Vector2(0,0);
