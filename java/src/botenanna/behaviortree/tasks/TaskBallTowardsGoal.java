a230eaa29e59f014c7419c7534fa4e80111be5e6
Hindsgaul <mhinds17@student.aau.dk>
2018-03-20 10:57:00 +0100

    Made new changes

--- java/src/botenanna/behaviortree/tasks/TaskBallTowardsGoal.java
+++ java/src/botenanna/behaviortree/tasks/TaskBallTowardsGoal.java
@@ -33,7 +33,7 @@ public class TaskBallTowardsGoal extends Leaf {
         // TODO Else try the difference of acceleration on car and ball vector with directions(and distance), and multiply/divide with seconds the predict
         // TODO If balls vector towards goal is bad adjust car before shooting.
 
-        double predictSeconds = (input.ballVelocity.getMagnitude()/input.myVelocity.getMagnitude())*(input.myDistanceToBall/1800);
+        double predictSeconds; //= (input.ballVelocity.getMagnitude()/input.myVelocity.getMagnitude())*(input.myDistanceToBall/1800);
 
         //double predictSeconds = (input.myDistanceToBall/2200);
 
@@ -41,17 +41,17 @@ public class TaskBallTowardsGoal extends Leaf {
        //     predictSeconds = 0.1;
        // }
 
-        if (predictSeconds > 5) {
+        //if (predictSeconds > 5) {
             predictSeconds = 5;
-       }
+       //}
 
-        if (1 < input.angleToBall || input.angleToBall < -1) {
+        //if (1 < input.angleToBall || input.angleToBall < -1) {
             predictSeconds = 0.1;
-        }
+       // }
 
-        if (input.myDistanceToBall < 300) {
+       // if (input.myDistanceToBall < 300) {
             predictSeconds = 0;
-        }
+      //  }
 
         double predict = 0.1;
         predictSeconds = 0;
