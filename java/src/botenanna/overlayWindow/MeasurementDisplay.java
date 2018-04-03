7296f44322549e7315b10d20b64a32e77ffeb7b1
Hindsgaul <mhinds17@student.aau.dk>
2018-04-03 11:37:12 +0200

    testing

--- java/src/botenanna/overlayWindow/MeasurementDisplay.java
+++ java/src/botenanna/overlayWindow/MeasurementDisplay.java
@@ -3,8 +3,13 @@ package botenanna.overlayWindow;
 import botenanna.AgentInput;
 import botenanna.Ball;
 import botenanna.Bot;
+import botenanna.physics.TimeTracker;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.HBox;
@@ -17,6 +22,9 @@ public class MeasurementDisplay extends VBox {
     public static final Color HEADER_COLOR = new Color(0.7, 0.7, 0.7, 1);
 
     private Label infoLabel;
+    private TimeTracker time;
+    private double timeSinceStart = 0;
+    private TextField timeInfo = new TextField("0");
 
     public MeasurementDisplay() {
         super();
@@ -25,19 +33,33 @@ public class MeasurementDisplay extends VBox {
         header.setPadding(new Insets(3, 5, 3, 5));
         header.setBackground(new Background(new BackgroundFill(HEADER_COLOR, null, null)));
 
+        Button button = new Button();
+        button.setText("Start timer");
+
+
+        button.setOnMouseClicked(event -> {
+            button.setText("Stop Timer");
+            time.startTimer();
+        });
+
+
         Label headerLabel = new Label("Car Velocity");
         header.getChildren().add(headerLabel);
+        //timeSinceStart = time.getElapsedSecondsTimer();
+        //timeInfo.setText(Double.toString((timeSinceStart)));
 
         infoLabel = new Label("No data");
         infoLabel.setPadding(new Insets(2, 3, 4, 10));
         infoLabel.setFont(new Font("Courier New", 14));
         getChildren().add(infoLabel);
+        getChildren().add(button);
+        //getChildren().add(timeInfo);
     }
 
     public void update(AgentInput input) {
         if (input == null)
             return;
-
+        TimeTracker time = new TimeTracker();
         AgentInput.Car car = input.myCar;
         infoLabel.setText(
                 "Vel: %f\n" +
