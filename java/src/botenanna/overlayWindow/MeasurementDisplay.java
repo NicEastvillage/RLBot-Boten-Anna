b9a8d5689921bba7de08de0f95a7dc057c8fcb02
Hindsgaul <mhinds17@student.aau.dk>
2018-04-05 12:49:50 +0200

    Added tool to export time and velocity of by a keypress or button

--- java/src/botenanna/overlayWindow/MeasurementDisplay.java
+++ java/src/botenanna/overlayWindow/MeasurementDisplay.java
@@ -1,15 +1,12 @@
 package botenanna.overlayWindow;
 
 import botenanna.AgentInput;
-import botenanna.Ball;
-import botenanna.Bot;
 import botenanna.physics.TimeTracker;
-import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.scene.input.MouseEvent;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.HBox;
@@ -17,16 +14,23 @@ import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 
+import java.io.*;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
 public class MeasurementDisplay extends VBox {
 
     public static final Color HEADER_COLOR = new Color(0.7, 0.7, 0.7, 1);
 
     private Label infoLabel;
-    private TimeTracker time;
-    private double timeSinceStart = 0;
-    private TextField timeInfo = new TextField("0");
+    private TimeTracker time = new TimeTracker();
+    private AgentInput.Car car;
 
-    public MeasurementDisplay() {
+    public MeasurementDisplay() throws FileNotFoundException {
         super();
         HBox header = new HBox();
         getChildren().add(header);
@@ -34,35 +38,80 @@ public class MeasurementDisplay extends VBox {
         header.setBackground(new Background(new BackgroundFill(HEADER_COLOR, null, null)));
 
         Button button = new Button();
-        button.setText("Start timer");
+        button.setText("Start Timer");
 
+        ScheduledExecutorService timer;
+        Runnable timerRunnable;
+        Label headerLabel;
+
+        timer = new ScheduledThreadPoolExecutor(1);
+        NumberFormat nf = new DecimalFormat("##.#");
+        timerRunnable = () -> {
+            System.out.println(time.getElapsedSecondsTimer());
+            try (PrintWriter out = new PrintWriter(new FileWriter("/Users/mathiashindsgaul/Desktop/velocity.txt",true))) {
+                out.println("Time: \t" + nf.format(time.getElapsedSecondsTimer()) + "\t\tVelocity: \t "); //+ nf.format(car.velocity.getMagnitude()));
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+        };
 
         button.setOnMouseClicked(event -> {
-            button.setText("Stop Timer");
             time.startTimer();
+            button.setText("Reset Timer");
+            final ScheduledFuture<?> timerHandle = timer.scheduleAtFixedRate(
+                    timerRunnable, 0, 1, TimeUnit.SECONDS);
+            timer.schedule(() -> {
+                timerHandle.cancel(true);
+            }, 5, TimeUnit.SECONDS);
+            try (PrintWriter out = new PrintWriter(new FileWriter("/Users/mathiashindsgaul/Desktop/velocity.txt",true))) {
+                out.println("");
+                out.println("Instance");
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
         });
 
+        addEventFilter(KeyEvent.KEY_PRESSED,event -> {
+            if(event.getCode() == KeyCode.L) {
+                time.startTimer();
+                button.setText("Reset Timer");
+                final ScheduledFuture<?> timerHandle = timer.scheduleAtFixedRate(
+                        timerRunnable, 0, 1, TimeUnit.SECONDS);
+                        timer.schedule(() -> {
+                            timerHandle.cancel(true);
+                            }, 5, TimeUnit.SECONDS);
+                try (PrintWriter out = new PrintWriter(new FileWriter("/Users/mathiashindsgaul/Desktop/velocity.txt",true))) {
+                    out.println("");
+                    out.println("Instance");
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
 
-        Label headerLabel = new Label("Car Velocity");
+        headerLabel = new Label("Car Velocity");
         header.getChildren().add(headerLabel);
-        //timeSinceStart = time.getElapsedSecondsTimer();
-        //timeInfo.setText(Double.toString((timeSinceStart)));
+
 
         infoLabel = new Label("No data");
         infoLabel.setPadding(new Insets(2, 3, 4, 10));
         infoLabel.setFont(new Font("Courier New", 14));
         getChildren().add(infoLabel);
         getChildren().add(button);
-        //getChildren().add(timeInfo);
+
     }
 
     public void update(AgentInput input) {
         if (input == null)
             return;
-        TimeTracker time = new TimeTracker();
-        AgentInput.Car car = input.myCar;
-        infoLabel.setText(
-                "Vel: %f\n" +
-                 car.velocity.getMagnitude());
+        if (input.myCar.playerIndex == 0) {
+            car = input.myCar;
+            infoLabel.setText(
+                    "Vel: %f\n" +
+                            car.velocity.getMagnitude());
+        }
     }
 }
