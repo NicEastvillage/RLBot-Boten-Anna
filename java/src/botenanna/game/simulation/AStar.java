0312dd5f02ad73ba05c30dfe5397c8d2166d44ca
NicEastvillage <nicoesterby@gmail.com>
2018-04-06 09:22:39 +0200

    Renamed action mutation to getFollowingActionSets.

--- java/src/botenanna/game/simulation/AStar.java
+++ java/src/botenanna/game/simulation/AStar.java
@@ -11,7 +11,7 @@ public class AStar {
     /** This method will generate all valid ActionSet that sensibly follow a given ActionSet in a Situation.
      * @param situation the Situation.
      * @param current the ActionSet prior to the ones generated. */
-    public List<ActionSet> mutateAction(Situation situation, ActionSet current) {
+    public List<ActionSet> getFollowingActionSets(Situation situation, ActionSet current) {
         /* Variabler:
         ///////// Optimizations taken
         [-1, 1] variabler behøver ikke reale tal
@@ -27,11 +27,11 @@ public class AStar {
         List<ActionSet> following = new LinkedList<>();
         following.add(current);
 
-        double[] newThrottles = mutateDirection(current.getThrottle());
-        double[] newSteerings = mutateDirection(current.getSteer());
+        double[] newThrottles = getFollowingDirections(current.getThrottle());
+        double[] newSteerings = getFollowingDirections(current.getSteer());
         // pitch and roll is 0, if car is grounded
-        double[] newPitches = situation.myCar.isMidAir ? mutateDirection(current.getPitch()) : new double[]{0};
-        double[] newRolls = situation.myCar.isMidAir ? mutateDirection(current.getRoll()) : new double[]{0};
+        double[] newPitches = situation.myCar.isMidAir ? getFollowingDirections(current.getPitch()) : new double[]{0};
+        double[] newRolls = situation.myCar.isMidAir ? getFollowingDirections(current.getRoll()) : new double[]{0};
         // jump is false, if jumping has no effect // FIXME With current implementation, second jump will always be one step long
         boolean[] newJumps = !situation.myCar.hasDoubleJumped ? new boolean[]{true, false} : new boolean[]{false};
         // boost is false, if car has no boost
@@ -71,7 +71,7 @@ public class AStar {
     }
 
     /** A helper method that returns an array of directions that are close to a given direction. */
-    private double[] mutateDirection(double value) {
+    private double[] getFollowingDirections(double value) {
         if (value == 1) return new double[]{1, 0};
         if (value == -1) return new double[]{0, -1};
         if (value == 0) return new double[]{1, 0, -1};
