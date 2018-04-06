d749793b1ea48a50298e8e47c42feb27c81bd100
Mikkel Kuntz <flintfedt-trash@hotmail.com>
2018-04-06 08:44:33 +0200

    Added fitness AriveAtPointAtTime class

--- java/src/botenanna/fitness/fitnessDriveOverPointWithAngle.java
+++ java/src/botenanna/fitness/fitnessDriveOverPointWithAngle.java
@@ -4,20 +4,19 @@ import botenanna.AgentInput;
 import botenanna.math.Vector3;
 
 /** This class is used when you want a fitness value for "Arrive at a point with a specific angle. */
-public class fitnessArriveWithAngle implements fitnessInterface {
+public class fitnessDriveOverPointWithAngle implements fitnessInterface {
 
-    public static final int DIST_SCALE = 450;
-    public static final double ANGLE_SCALE = 5.09299;
+    private final int DIST_SCALE = 450;
+    private final double ANGLE_SCALE = 5.09299;
 
-    public double angleDeviation;
-    public double distDeviation;
-
-    public Vector3 point;
+    private double angleDeviation;
+    private double distDeviation;
+    private Vector3 point;
 
      /**@param point the destination point.
      *  @param angleDeviation an value that the angle is allowed to deviate.
      *  @param distDeviation an value that the distance is allowed to deviate. */
-    public fitnessArriveWithAngle(Vector3 point, double angleDeviation, double distDeviation) {
+    public fitnessDriveOverPointWithAngle(Vector3 point, double angleDeviation, double distDeviation) {
         this.point = point;
         this.angleDeviation = angleDeviation;
         this.distDeviation = distDeviation;
@@ -31,21 +30,11 @@ public class fitnessArriveWithAngle implements fitnessInterface {
     public double calculateFitness(AgentInput situation, double timeSpent){
 
         //Calculate function variables
-        double angToPoint = situation.myCar.position.getAngleTo(point); //ang
-        double distToPoint = situation.myCar.position.getDistanceTo(point); //dist
+        double angToPoint = situation.myCar.position.getAngleTo(point); // Angle
+        double distToPoint = situation.myCar.position.getDistanceTo(point); // Distance
 
-        //Calculate the fitness function
-        return calculateFormula(timeSpent, angToPoint * ANGLE_SCALE, distToPoint * DIST_SCALE);
-    }
-
-    /** Calculate the fitness.
-     *  @param timeSpent the time spend since origin.
-     *  @param angScaled angle to a point scaled.
-     *  @param distScaled distance to a point scaled.
-     *  @return the fitness value*/
-    @Override
-    public double calculateFormula(double timeSpent, double angScaled, double distScaled){
-        return Math.pow(Math.E, -(timeSpent + Math.abs(angScaled) + distScaled));
+        //Calculate and return the fitness
+        return Math.pow(Math.E, -(timeSpent + Math.abs(angToPoint * ANGLE_SCALE) + (distToPoint * DIST_SCALE)));
     }
 
     /** Checks if the deviations are fulfilled.
@@ -56,8 +45,8 @@ public class fitnessArriveWithAngle implements fitnessInterface {
     public boolean isDeviationFulfilled(AgentInput situation, double timeSpent) {
 
         //Calculate function variables
-        double distToPoint = situation.myCar.position.getDistanceTo(point); //dist
-        double angToPoint = situation.myCar.position.getAngleTo(point); //ang
+        double distToPoint = situation.myCar.position.getDistanceTo(point); // Distance
+        double angToPoint = situation.myCar.position.getAngleTo(point); // Angle
 
         if(distToPoint <= distDeviation){
             if(angToPoint <= angleDeviation)
--- java/src/botenanna/fitness/fitnessInterface.java
+++ java/src/botenanna/fitness/fitnessInterface.java
@@ -7,9 +7,6 @@ public interface fitnessInterface {
     /** Used to calculate the fitness value. */
     double calculateFitness(AgentInput situation, double timeSpent);
 
-    /** Help method for calculateFitness. Calculates the fitness with a formula. */
-    double calculateFormula(double timeSpent, double angScaled, double distScaled);
-
     /** Checks if the deviations are fulfilled. */
     boolean isDeviationFulfilled(AgentInput situation, double timeSpent);
 }
